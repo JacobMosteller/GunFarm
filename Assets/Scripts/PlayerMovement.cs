b4f8cbb48d2a442ba3378c3e0efa55038f6133e5
Jacob Mosteller <46802543+JacobMosteller@users.noreply.github.com>
2024-08-31 12:29:40 -0600

    walk in circles
    to do:
    catch tail

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float vertical;
     [SerializeField] float speed;
     [SerializeField] Rigidbody2D rb;
+    [SerializeField] Animator animator;
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,11 +22,53 @@ public class PlayerMovement : MonoBehaviour
     {
         vertical = Input.GetAxisRaw("Horizontal");
         horizontal = Input.GetAxisRaw("Vertical");
+        SpriteMover();
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
         rb.velocity = new Vector2(vertical * speed, rb.velocity.x);
+        
     }
+
+    void SpriteMover()
+    {
+        if (horizontal == 0 &&  vertical == 0)
+        {
+            animator.SetBool("walkingForwards", false);
+            animator.SetBool("walkingAway", false);
+            animator.SetBool("walkingRight",false);
+            animator.SetBool("walkingLeft", false);
+                }
+        if (horizontal == 1 && vertical == 0)
+        {
+            animator.SetBool("walkingForwards",false);
+            animator.SetBool("walkingAway",true);
+            animator.SetBool("walkingRight",false);
+            animator.SetBool("walkingLeft",false);
+                }
+        if (horizontal == -1 && vertical == 0)
+        {
+            animator.SetBool("walkingForwards",true);
+            animator.SetBool("walkingAway",false);
+            animator.SetBool("walkingRight",false);
+            animator.SetBool("walkingLeft",false);
+                }
+        if (horizontal == 0 && vertical == 1)
+        {
+            animator.SetBool("walkingForwards",false);
+            animator.SetBool("walkingAway",false);
+            animator.SetBool("walkingRight",true);
+            animator.SetBool("walkingLeft",false);
+                }
+        if (horizontal == 0 && vertical == -1)
+        {
+            animator.SetBool("walkingForwards",false);
+            animator.SetBool("walkingAway",false);
+            animator.SetBool("walkingRight",false);
+            animator.SetBool("walkingLeft",true);
+                }
+    }
+
 }
