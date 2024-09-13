ca2baea35a4ca4689235231e11877769d31e07c6
Jacob Mosteller <46802543+JacobMosteller@users.noreply.github.com>
2024-09-12 21:09:00 -0600

    HES GOT A GUN

---
+++ Assets/Scripts/GunRotation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunRotation : MonoBehaviour
+{
+    [SerializeField] Transform rotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
