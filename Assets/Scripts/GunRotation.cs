573453a15cd1f5db31bcd4419c19fc3450be7a24
Jacob Mosteller <46802543+JacobMosteller@users.noreply.github.com>
2024-09-13 00:19:33 -0600

    LOOK OUT HE CAN POINT AT YOU

--- Assets/Scripts/GunRotation.cs
+++ Assets/Scripts/GunRotation.cs
@@ -1,19 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GunRotation : MonoBehaviour
 {
-    [SerializeField] Transform rotation;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Transform m_transform;
+    [SerializeField] Transform other;
+    private SpriteRenderer m_renderer;
 
-    // Update is called once per frame
+
+    private void Start()
+    {
+        m_transform = this.transform;
+        m_renderer = this.GetComponent<SpriteRenderer>();
+    }
     void Update()
     {
-        
+        LAMouse();
     }
+
+    private void LAMouse()
+    {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - m_transform.position;
+
+        float angle = Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg;
+
+        Quaternion rotation = Quaternion.AngleAxis(angle , Vector3.forward);
+        m_transform.rotation = rotation;
+
+        if (angle < -90 || angle > 90)
+        {
+            m_transform.localRotation = Quaternion.Euler(180, 0, -angle);
+        }
+    }
+   
 }
